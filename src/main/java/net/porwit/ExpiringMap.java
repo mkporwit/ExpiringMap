13aa0336c33713b997452bc81bc956e28d5ead4e
Marcin Krzysztof Porwit <mkporwit@porwit.net>
2016-07-28 17:35:18 -0700

    Fleshed out tests and implementation using priority queue and background cleanup thread

--- src/main/java/net/porwit/ExpiringMap.java
+++ src/main/java/net/porwit/ExpiringMap.java
@@ -1,40 +1,113 @@
 package net.porwit;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ExpiringMap<K, V>
 {
-	private HashMap<K, Value> map = new HashMap<K, Value>();
+	private ConcurrentHashMap<K, Value> map = new ConcurrentHashMap<K, Value>();
+	private PriorityQueue<Key> pq = new PriorityQueue<Key>();
+	
+	private Timer cleanupTimer;
+	
+	public ExpiringMap(long cleanupMillis) {
+		cleanupTimer = new Timer();
+		cleanupTimer.scheduleAtFixedRate(new CleanupTask(this), cleanupMillis, cleanupMillis);
+	}
+	
+	private class Key implements Comparable<Key>{
+		protected K key;
+		protected long expiryMillis;
+		
+		public Key(K key, long expiryMillis) {
+			this.key = key;
+			this.expiryMillis = expiryMillis;
+		}
 
-	private final long DEFAULT_LIFE_MILLIS = 5000;
-
-	private class Value<V> {
+		@Override
+		public int compareTo(Key k) {
+			if(this.expiryMillis < k.expiryMillis) {
+				return -1;
+			} else if(this.expiryMillis > k.expiryMillis) {
+				return 1;
+			}
+			return 0;
+		}
+	}
+	
+	public int size() {
+		return map.size();
+	}
+	
+	private class Value {
 		protected V val;
 		protected long expiryMillis;
 
-		public Value(V value, long lifeMillis) {
+		public Value(V value, long expiryMillis) {
 			val = value;
-			expiryMillis = System.currentTimeMillis() + lifeMillis;
+			this.expiryMillis = expiryMillis;
 		}
 	}
 
 	public V get(K key) {
+		Value v = map.get(key);
+		if(v != null) {
+			long curTime = System.currentTimeMillis();
+			if(curTime <= v.expiryMillis) {
+				return v.val;
+			} else {
+				return null;
+			}
+		}
 		return null;
-	}
-
-	public V put(K key, V value) {
-		return put(key, value, DEFAULT_LIFE_MILLIS);
 	}
 
 	public V put(K key, V value, long lifeMillis) {
-		Value<V> v = new Value<V>(value, lifeMillis);
-		Value<V> ret = map.put(key, v);
-		System.out.println("ret is " + ret == null ? "null" : ret);
-		System.out.println("ret.val is " + ret.val == null ? "null" : ret.val);
-		if(ret != null)
-		{
+		long expiryMillis = System.currentTimeMillis() + lifeMillis;
+		Value v = new Value(value, expiryMillis);
+		Key k = new Key(key, expiryMillis);
+		pq.add(k);
+		Value ret = map.put(key, v);
+		if(ret == null) {
+			return null;
+		} else {
 			return ret.val;
-		} 
-		return null;
+		}
+	}
+	
+	public void cleanup() {
+		Key k = pq.peek();
+		Set<Key> delSet = new HashSet<>();
+		long curTime = System.currentTimeMillis();
+		while(k != null && k.expiryMillis < curTime) {
+			delSet.add(pq.remove());
+			k = pq.peek();
+		}
+		
+		Iterator<ExpiringMap<K, V>.Key> keyIter = delSet.iterator();
+		while(keyIter.hasNext()) {
+			k = keyIter.next();
+			map.remove(k.key);
+		}
+	}
+	
+	class CleanupTask extends TimerTask {
+
+		ExpiringMap<K, V> map;
+		
+		public CleanupTask(ExpiringMap<K, V> expiringMap) {
+			map = expiringMap;
+		}
+
+		@Override
+		public void run() {
+			map.cleanup();
+		}
+		
 	}
 }
--- src/test/java/net/porwit/ExpiringMapTest.java
+++ src/test/java/net/porwit/ExpiringMapTest.java
@@ -3,6 +3,8 @@ package net.porwit;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Unit test for ExpiringMap
@@ -10,21 +12,93 @@ import static org.junit.Assert.assertEquals;
 public class ExpiringMapTest
 {
     @Test
-    public void simplePutTest()
-    {
-	    ExpiringMap<String, String> map = new ExpiringMap<>();
+    public void simplePutTest() {
+	    ExpiringMap<String, String> map = new ExpiringMap<>(1000);
 	    String value = "testValue1";
-	    String ret = map.put("testKey1", value);
+	    String ret = map.put("testKey1", value, 5000);
 	    assertTrue("simple put works", ret == null);
     }
 
     @Test
-    public void replacementPutTest()
-    {
-	    ExpiringMap<String, String> map = new ExpiringMap<>();
+    public void replacementPutTest() {
+	    ExpiringMap<String, String> map = new ExpiringMap<>(1000);
 	    String value = "testValue1";
-	    String ret = map.put("testKey1", value);
-	    ret = map.put("testKey1", value);
-	    assertEquals("replacement works", value.equals(ret));
+	    String ret = map.put("testKey1", value, 5000);
+	    ret = map.put("testKey1", value, 5000);
+	    assertEquals("replacement works", value, ret);
+    }
+    
+    @Test
+    public void simpleGetTest() {
+    	ExpiringMap<String, String> map = new ExpiringMap<>(1000);
+    	String value = "testValue1";
+    	String ret = map.put("testKey1", value, 5000);
+    	
+    	ret = map.get("testKey1");
+    	assertNotNull("ret is not null", ret);
+    	assertEquals("valid return", value, ret);
+    }
+    
+    @Test
+    public void failedGetTest() {
+    	ExpiringMap<String, String> map = new ExpiringMap<>(1000);
+    	String ret = map.get("testKey1");
+    	assertNull("ret is null", ret);
+    }
+    
+    @Test
+    public void expiryGetSuccessTest() throws InterruptedException {
+    	ExpiringMap<String, String> map = new ExpiringMap<>(1000);
+    	long timeout = 1000;
+    	String value = "testValue1";
+    	String ret = map.put("testKey1", value, timeout);
+    	Thread.sleep(timeout + 1);
+    	ret = map.get("testKey1");
+    	assertNull("ret is null", ret);
+    }
+    
+    @Test
+    public void synchronousCleanupTest() throws InterruptedException {
+    	ExpiringMap<String, String> map = new ExpiringMap<>(1000);
+    	long timeout = 500;
+    	map.put("testKey1", "testValue1", timeout);
+    	map.put("testKey2", "testValue2", timeout);
+    	map.put("testKey3", "testValue3", timeout);
+    	timeout = 5000;
+    	map.put("testKey4", "testValue4", timeout);
+    	map.put("testKey5", "testValue5", timeout);
+    	map.put("testKey6", "testValue6", timeout);
+    	assertEquals("map has six elements", 6, map.size());
+    	Thread.sleep(500+1);
+    	map.cleanup();
+    	assertEquals("map has three elements", 3, map.size());
+    	Thread.sleep(timeout + 1);
+    	map.cleanup();
+    	assertEquals("map has zero elements", 0, map.size());
+    }
+    
+    @Test
+    public void emptyMapCleanupTest() {
+    	ExpiringMap<String, String> map = new ExpiringMap<>(1000);
+    	map.cleanup();
+    	assertEquals("map has zero elements", 0, map.size());
+    }
+    
+    @Test
+    public void timerBasedCleanupTest() throws InterruptedException {
+    	ExpiringMap<String, String> map = new ExpiringMap<>(1000);
+    	long timeout = 500;
+    	map.put("testKey1", "testValue1", timeout);
+    	map.put("testKey2", "testValue2", timeout);
+    	map.put("testKey3", "testValue3", timeout);
+    	timeout = 5000;
+    	map.put("testKey4", "testValue4", timeout);
+    	map.put("testKey5", "testValue5", timeout);
+    	map.put("testKey6", "testValue6", timeout);
+    	assertEquals("map has six elements", 6, map.size());
+    	Thread.sleep(2000+1);
+    	assertEquals("map has three elements", 3, map.size());
+    	Thread.sleep(timeout + 1);
+    	assertEquals("map has zero elements", 0, map.size());
     }
 }
